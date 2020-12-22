26f4778c8f263de94c0da1a15d2b191a6cd93bb4
Harsh Parekh <harsh_parekh@outlook.com>
2020-12-21 21:10:39 -0500

    Update autostart to discard errors.

--- .config/dwm/autostart.sh
+++ .config/dwm/autostart.sh
@@ -13,23 +13,23 @@ nitrogen --restore
 xset r rate 275
 
 ### Enable Tap-to-click
-xinput set-prop "SynPS/2 Synaptics TouchPad" "libinput Tapping Enabled" 1
+# xinput set-prop "SynPS/2 Synaptics TouchPad" "libinput Tapping Enabled" 1
 
 ### Run dunst notification daemon
-killall dunst
+killall dunst 2> /dev/null
 (cd $HOME && dunst) &
 
 ### Run Simple X Hot-Key Daemon
-killall sxhkd
+killall sxhkd 2> /dev/null
 (cd "$HOME" && . ./.config/sh/.shrc && sxhkd) &
 
 ### Populate status info
-killall slstatus
+killall slstatus 2> /dev/null
 slstatus &
 
 ### Setup ssh-agent
 # It works as follows:
 # Start the agent and store the setup code to .cache
 # shrc will source the setup code and things should work.
-killall ssh-agent
+killall ssh-agent 2> /dev/null
 ssh-agent -s | head -n-1 > "$HOME"/.cache/ssh/agent &
